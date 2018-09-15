6853b248461729b2708cc87309f129b62eaa5c87
jggross <gross.justin8@gmail.com>
2018-09-15 16:36:10 -0700

    Create insert popup function to hide a popper in the page. Create translateId function to map ids to specific animals. Add popper to show up above highlighted text on mouse over

--- main.js
+++ main.js
@@ -1,21 +1,53 @@
 var endangered_animals = ["African Elephant", "Woolly Mammoth", "panda"]
+var page_content
+var enfo_popper
+var popperNode
 
+
+
+//ANYTIME YOU NEED TO USE AN ID FOR AN ANIMAL CALL THIS
+function translateId(animal) {
+    return animal.replace(/ /g, '')
+}
 
 function showModal(animal) {
-    console.log(`${animal} has been clicked!`)
+    console.log(`${translateId(animal)} has been clicked!`)
     alert(`${animal} clicked!`)
+    referenceObject = $(`#${translateId(animal)}`)
+    popperNode = $('#enfo_popup')
+    enfo_popper = new Popper(referenceObject, popperNode, {
+        placement: 'top',
+        
+        modifiers: {
+            flip: {
+                behavior: ['left', 'right', 'top', 'bottom']
+            },
+            offset: {
+                enabled: true,
+                offset: '0,10'
+            }
+        }
+    });
+}
+
+//Append the popup element to the body
+function insert_popup() {
+    page_content = `<div id="enfo_popup"><p>Popup Test</p></div>`
+    page_content = document.body.innerHTML.concat(page_content)
+    $("body").html(page_content);
 }
 
 function highlight_species() {
     endangered_animals.forEach(function (animal) {
-        var page_content = document.body.innerHTML.replace(new RegExp(animal, "gi"), `<span id='${animal}' class='enfo' style='background-color: yellow'>${animal}</span>`)
+        page_content = document.body.innerHTML.replace(new RegExp(animal, "gi"), `<span id='${translateId(animal)}' class='enfo' style='background-color: yellow'>${animal}</span>`)
         $("body").html(page_content);
     });
 }
 
+insert_popup()
 highlight_species()
 
 
 $(document).ready(function () {
-    $(".enfo").hover(function() { showModal($(this).attr("id")) } )
+    $(".enfo").hover(function () { showModal($(this).attr("id")) })
 })
