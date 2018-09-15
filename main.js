c5fc409a189c86258df8f8604d5e4fd44f930b67
jggross <gross.justin8@gmail.com>
2018-09-15 13:35:43 -0700

    Create highlight species function to highlight all species within a given array

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-var page_content = document.body.innerHTML.replace(/african elephant/ig, "<span style='background-color: yellow'>African Elephant</span>");
-$("body").html(page_content);
+var endangered_animals = ["African Elephant", "Woolly Mammoth", "panda"]
+
+function highlight_species() {
+    endangered_animals.forEach(function (animal) {
+        var page_content = document.body.innerHTML.replace(new RegExp(animal, "gi"), `<span onclick='showModal()' style='background-color: yellow'>${animal}</span>`)
+        $("body").html(page_content);
+    });
+}
+
+highlight_species()
+
+
+
+
+
+
