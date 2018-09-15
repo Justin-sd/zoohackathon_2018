a777dcc52b39f13e45a7223a9f60830c5e0ab6f0
jggross <gross.justin8@gmail.com>
2018-09-15 15:10:51 -0700

    Change click to hover

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 var endangered_animals = ["African Elephant", "Woolly Mammoth", "panda"]
 
+
+function showModal(animal) {
+    console.log(`${animal} has been clicked!`)
+    alert(`${animal} clicked!`)
+}
+
 function highlight_species() {
     endangered_animals.forEach(function (animal) {
-        var page_content = document.body.innerHTML.replace(new RegExp(animal, "gi"), `<span onclick='showModal()' style='background-color: yellow'>${animal}</span>`)
+        var page_content = document.body.innerHTML.replace(new RegExp(animal, "gi"), `<span id='${animal}' class='enfo' style='background-color: yellow'>${animal}</span>`)
         $("body").html(page_content);
     });
 }
@@ -10,7 +16,6 @@ function highlight_species() {
 highlight_species()
 
 
-
-
-
-
+$(document).ready(function () {
+    $(".enfo").hover(function() { showModal($(this).attr("id")) } )
+})
