2383b03d9f5454cd232a10c9f36cd83322e40163
Tiffany Yang <teefyang@Tiffanys-MacBook-Pro.local>
2018-09-15 17:11:04 -0700

    refresh from justin's popover

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
+
+let ref;
+let popper;
+// let modal = $.get(chrome.extension.getURL('/template.html'), function(data) {
+//     $(data).appendTo('body');
+//     // Or if you're using jQuery 1.8+:
+//     // $($.parseHTML(data)).appendTo('body');
+// });
+                        
+
 var endangered_animals = ["African Elephant", "Woolly Mammoth", "panda"]
 
 
+
+
 function showModal(animal) {
+    ref = $('#id');
+
+    popper = new Popper(ref,popup,{
+        placement: 'top',
+    });
+
+
     console.log(`${animal} has been clicked!`)
     alert(`${animal} clicked!`)
 }
@@ -17,5 +36,10 @@ highlight_species()
 
 
 $(document).ready(function () {
-    $(".enfo").hover(function() { showModal($(this).attr("id")) } )
+    let temp = "hover";
+    let hoverid;
+    $(".enfo").hover(function() { 
+        hoverid = $(this).attr("class",temp ); 
+        showModal(hoverid) 
 })
+});
