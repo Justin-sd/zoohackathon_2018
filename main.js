09bed562e4d826e610180f16b9df1fa3274a9850
jggross <gross.justin8@gmail.com>
2018-09-16 00:16:10 -0700

    Add blank default values for populate modal function and change the display of modal to none when it is first added or not being used

--- main.js
+++ main.js
@@ -13,7 +13,9 @@ function translateId(animal) {
 
 function showModal(animal) {
     referenceObject = $(`#${translateId(animal)}`)
+    //$(`#${animal}`).css("display", "block")
     popperNode = $('#enfo_popup')
+    popperNode.css("display", "block")
 
     enfo_popper = new Popper(referenceObject, popperNode, {
         placement: 'top',
@@ -30,6 +32,7 @@ function showModal(animal) {
 }
 
 function hideModal() {
+    popperNode.css("display", "none")
     enfo_popper.destroy()
 }
 
@@ -38,6 +41,7 @@ function insert_popup() {
     page_content = `<div id="enfo_popup" style="background-color: white;
   margin: auto;
   padding: 20px;
+  display: none;
   border: 1px solid #888;
   width: 30%;" class="modal"><div class="modal-content"><img id="enfo_popup_animal_img" height="200px" width="200px" src=""></img><img id="enfo_popup_stats_img" width="100px" height="100px" src=""><p id="enfo_popup_text">placeholder text</p></div></div>`
     page_content = document.body.innerHTML.concat(page_content);
@@ -59,6 +63,11 @@ function populateModal(animal_id) {
             graph_img = ""
             blurb = "The African bush elephant (Loxodonta africana), also known as the African savanna elephant, is the larger of the two species of African elephants, and the largest living terrestrial animal. These elephants were previously regarded as the same species, but the African forest elephant has been reclassified as L. cyclotis."
             break
+        default:
+            endangerment_level = ""
+            animal_img = ""
+            graph_img = ""
+            blurb = ""
     }
 
     $("#enfo_popup").css("background", endangerment_level)
